6881fb33a50f8a2b4ec7635a52a4c300d0f66de0
Leandro Luiz Pereira <leandro.pereira.moon@hotmail.com>
2023-12-29 13:39:13 -0300

    create

--- Códigos-Exemplos/function.js
+++ Códigos-Exemplos/function.js
@@ -7,7 +7,7 @@ alert(msg);
 // Declaração de variáveis 
 
 var n = 50 ;
-var y = 30 ;
+var y = 33 ;
 var x = 4.3;
 var nome ="leandro ";
 var bolean = true ;
@@ -20,6 +20,38 @@ console.log("Operadores Aritméticos");
 
 var soma = n + y ;
 
-console.log("Soma de "+n+" + "+y+" ="+soma);
+console.log(" Soma de "+n+" + "+y+" = "+soma);
 
+var sub = n - y ;
+
+console.log(" Subtração de "+n+" - "+y+" = "+sub);
+
+var div = n / y ;
+
+console.log(" Divisão de "+n+" / "+y+" = "+sub);
+
+var mod = n % 2  ;
+
+console.log(" Resto da divisão de "+n+" / 2 é  = "+mod);
+
+
+
+// verificar se é par ou immpar
+
+if(n % 2==0){
+ 
+ console.log(" o "+n+" divido por 2 o resto da divisão é 0 então é  par");
+}
+else{
+    console.log("o "+n+" divido por 2 o resto da divisão não é 0 então e impar");
+}
+
+
+
+if(y % 3 == 0 ){
+    console.log("o "+y+" divido por 2 o resto da divisão não é 0 então e impar");
+}
+else{
+    console.log(" o "+y+" divido por 2 o resto da divisão é 0 então é  par");
+}
 
