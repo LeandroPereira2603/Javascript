ee60bc41599771a1ef16c961df74a78b152b7558
Leandro Luiz Pereira <leandro.pereira.moon@hotmail.com>
2023-12-29 13:27:28 -0300

    creatae

---
+++ Códigos-Exemplos/function.js
@@ -0,0 +1,25 @@
+
+// primeiro código
+
+var alert = console.log("Hello , world !!");
+alert(alert);
+
+// Declaração de variáveis 
+
+var n = 50 ;
+var y = 30 ;
+var x = 4.3;
+var nome ="leandro ";
+var bolean = true ;
+var peso = 10.23 ;
+var caractere = 'a';
+
+// Operadores aritméticos 
+
+console.log("Operadores Aritméticos");
+
+var soma = n + y ;
+
+console.log("Soma de "+n+" + "+y+" ="+soma);
+
+
