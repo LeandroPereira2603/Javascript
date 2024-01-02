524d5c28fca26e04dd43d394aaf42f53c28e1e8f
Leandro Luiz Pereira <leandro.pereira.moon@hotmail.com>
2024-01-02 20:15:08 -0300

    create

---
+++ Códigos-Exemplos/Operadores Relacionais/function.js
@@ -0,0 +1,9 @@
+var altButterfly = 0.08 ;
+var altMariposa = 0.07;
+
+var logica = altButterfly > altMariposa ;
+
+console.log("A Butterfly tem altura  "+altButterfly);
+console.log("A mariposa tem a altura "+altMariposa);
+console.log (" A butterfly e mais alta que a mariposa ? : ");
+console.log(logica);
