dfa5808e870c30f849188b1207e8d0d68bb9ee27
Leandro Luiz Pereira <leandro.pereira.moon@hotmail.com>
2024-01-02 11:30:49 -0300

    create

---
+++ Códigos-Exemplos/Operadores Aritmético/function.js
@@ -0,0 +1,17 @@
+console.log("Operadores Aritméticos");
+
+var soma = n + y ;
+
+console.log(" Soma de "+n+" + "+y+" = "+soma);
+
+var sub = n - y ;
+
+console.log(" Subtração de "+n+" - "+y+" = "+sub);
+
+var div = n / y ;
+
+console.log(" Divisão de "+n+" / "+y+" = "+sub);
+
+var mod = n % 2  ;
+
+console.log(" Resto da divisão de "+n+" / 2 é  = "+mod);
