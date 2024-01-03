75844a3f6ec818214afaccef5510d77926ac4816
Leandro Luiz Pereira <leandro.pereira.moon@hotmail.com>
2024-01-02 21:42:37 -0300

    create

---
+++ Códigos-Exemplos/Concatenacao de variavel/function.js
@@ -0,0 +1,16 @@
+// Concatenação de variável 
+
+var nome ="Leandro"
+var sobrenome ="Pereira ";
+var nomeCompleto = nome = sobrenome ;
+
+console.log(nomeCompleto);
+
+var nOne = "20"
+var nTwo = "40";
+
+var somaConc = nOne + nTwo ;
+
+console.log(somaConc);
+
+// Irá retorna : 2040 
