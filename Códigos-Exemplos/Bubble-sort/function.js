a26b0588668c2cdee24be3e6fa44a9c2460e7ffd
Leandro Pereira ( LLP ) <154700613+LeandroPereira2603@users.noreply.github.com>
2024-01-20 22:55:37 -0300

    Atualizar o function.js

--- Códigos-Exemplos/Bubble-sort/function.js
+++ Códigos-Exemplos/Bubble-sort/function.js
@@ -1,87 +1,39 @@
+vet = [];
+vet.push(378,67,2377,10,8,7);
+console.log("\nNúmeros colocado nos indices :\n ")
+console.log(vet);
 
-/*
-
-# Bubble sort 
-
-Para a implementação serão usados dois laços Bubble . </p>
-
-*/
-
- var vet = [];
- vet.push(23,323,4324,234,645,34);
-
- function bubbleSort_1(items){
-    for(let j = 0; j > items.lenght - 1; j++){
-        for(let i = 0; i< items.lenght - 1;i++){
-            if(items[i] > items[i+1]){
-              [items[i] , items[i+1]] = [items[i+1], items[i]];
-            }
-        }
-    }
-
-    return items;
+function bubbleSort(vet){
+ for (var j = 0 ; j < vet.length;j++){
+  for (var i = 0 ; i < vet.length;i++){
+   if (vet[i] > vet[i+1]){
+    [vet[i] , vet[i+1]] = [vet[i+1],vet[i]];
+   }
+  }
  }
+ return vet;
+}
 
- console.log(bubbleSort_1(vet));
+function bubbleSortDecrecente(vet){
+ for (var j = 0 ; j < vet.length;j++){
+  for (var i = 0 ; i < vet.length;i++){
+   if (vet[i] < vet[i+1]){
+    [vet[i+1] , vet[i]] = [vet[i],vet[i+1]];
+   }
+  }
+}
 
- function bubbleSort_2(items){
-    for(let last = items.lenght - 1; last > 0;last--){
-        for(let i = 0; i< last; i++){
-            if(items[i] > items[i+1]){
-              [items[i] , items[i+1]] = [items[i+1] , items[i]];
-             
-            }
-        }
-    }
-
-    return items;
- }
-
- console.log(bubbleSort_2(vet));
-
- function bubbleSort_3(items){
-
-    let swap ;
-    let last = items.lenght - 1 ;
-    do {
-        swap = false;
-        for(let i = 0;i<last; i++){
-            if(items[i] > items[i + 1]){
-                [items[i], items[i+1]] = [items[i+1] , items[i]];
-                swap = true;
-            }
-        }
-
-      last--;
-
-    } while (swap);
-
-    return items;
- }
-
- console.log(bubbleSort_3(vet));
-
-
- const arr = [8, 20,-2,4,-6];
-
- function bubbleSort(arr){
-
-    let swapped ;
-
-    do {
-        swapped = false;
-    for(let i = 0;i <arr.lenght - 1; i++){
-        if(arr[i] > arr[i+1]) {
-            let temp = arr[i]
-            arr[i] = arr[i+1]
-            arr[1+1] = temp ;
-            swapped = true ;
-        }
-    }
-
- } while(swapped);
+return vet;
 
 }
 
-console.log(bubbleSort(arr));
+bubbleSort(vet);
 
+console.log("\nNúmeros crescente (Bubble Sort) :\n ")
+console.log(vet)
+
+bubbleSortDecrecente(vet);
+console.log("\nNúmeros decrescente (Bubble Sort) :\n ");
+console.log(vet);
+
+console.log("\nNúmeros totais de indices : "+vet.length);
